55de921bb10b5a60029ac096cced77ad38650446
Muhammad Arslan Tufail <user@ITRAMP-104>
2024-10-03 19:40:06 +0500

    Image-Detail-Page

--- src/component/main-gallery/main-gallery.js
+++ src/component/main-gallery/main-gallery.js
@@ -1,4 +1,7 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Tabs, Tab, TabScreen } from "react-tabs-scrollable";
+import "react-tabs-scrollable/dist/rts.css";
+
 const images=[
     { src: 'assets/img/img-1.png',slug:'skull.with.hair'},
     { src: 'assets/img/img-2.png',slug:'skull.with.hair'},
@@ -34,23 +37,86 @@ const images=[
 
 ]
 function MainGallery() {
+    const [imageDetail, setImageDetail] = useState(true);
+    const [activeTab, setActiveTab] = React.useState(1);
+    const onTabClick = (e, index) => {
+        setActiveTab(index);
+    };
     return(
         <main>
-            <div className='gallery-wrapper'>
-                {images.map((item) => (
-                    <div className='gallery-item position-relative' key={item.id}>
-                        <img src={item.src} />
-                        <div className='img-slug p-4 position-absolute bottom-0 d-flex h-100 w-100 align-items-end text-white cursor-pointer'>
-                            <p>{item.slug}</p>
+            {imageDetail ?
+            <div className='gallery-grid-wrapper'>
+                <div className='gallery-wrapper'>
+                    {images.map((item) => (
+                        <div className='gallery-item position-relative' key={item.id} onClick={()=>setImageDetail(false)}>
+                            <img src={item.src} />
+                            <div className='img-slug p-4 position-absolute bottom-0 d-flex h-100 w-100 align-items-end text-white cursor-pointer'>
+                                <p>{item.slug}</p>
+                            </div>
+                        </div>
+                    ))}
+                    
+                </div>
+                <div className='text-center see-more-images'>
+                    <hr width='130' className='border-2 m-auto mb-2'/>
+                    <h6>log in to see more</h6>
+                </div>
+            </div>
+             :
+                <div className='image-detail-wrapper'>
+                    <div className='detail-left-container'>
+                        <div className='image-wrapper'>
+                            {images.map((item,index) => (
+                                <TabScreen key={index} index={index} activeTab={activeTab}>
+                                    <div className="img-container">
+                                        <img src={item.src}/>
+                                    </div>
+                                </TabScreen>
+                            ))}
                         </div>
                     </div>
-                ))}
-                
-            </div>
-            <div className='text-center see-more-images'>
-                <hr width='130' className='border-2 m-auto mb-2'/>
-                <h6>log in to see more</h6>
-            </div>
+                    <div className='detail-right-container'>
+                    {images.map((item,index) => (
+                        <TabScreen key={index} index={index} activeTab={activeTab}>
+                            <div className="img-data">
+                                <p>{item.slug} 17h</p>
+                                <p className='my-3'>black cat sitting on an open book, surrounded by candles and spell books, in the background is grey wallpaper, fantasy art style painting</p>
+                                <ul className='list-unstyled d-flex flex-wrap gap-2 p-0 m-0'>
+                                    <li>
+                                        <span className='white-space-nowrap rounded py-1 px-2'>chaos 50</span>
+                                    </li>
+                                    <li>
+                                        <span className='white-space-nowrap rounded py-1 px-2'>ar 5:7</span>
+                                    </li>
+                                    <li>
+                                        <span className='white-space-nowrap rounded py-1 px-2'>style raw</span>
+                                    </li>
+                                    <li>
+                                        <span className='white-space-nowrap rounded py-1 px-2'>v 6.1</span>
+                                    </li>
+                                    <li>
+                                        <span className='white-space-nowrap rounded py-1 px-2'>stylize 1000</span>
+                                    </li>
+                                    <li>
+                                        <span className='white-space-nowrap rounded py-1 px-2'>personalize ap4xwe8</span>
+                                    </li>
+                                    
+                                </ul>
+                            </div>
+                        </TabScreen>
+                    ))}
+                    <Tabs activeTab={activeTab} onTabClick={onTabClick}>
+                        {images.map((item,index) => (
+                            <Tab key={index}>
+                                <div className="tab-img">
+                                    <img src={item.src}/>
+                                </div>
+                            </Tab>
+                        ))}
+                    </Tabs>
+                    </div>
+                </div>
+            }
         </main>
     )
 }
