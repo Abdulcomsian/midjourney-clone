49a804466b96d40b2871a1aacb7d5b6e090f4ada
aqibibrahim91 <aqibibrahimepggmail.com>
2024-10-26 10:32:28 +0500

    fix: remove rootlaout and remove default dark mode

--- app/layout.js
+++ app/layout.js
@@ -1,16 +1,43 @@
-'use client'
-// app/layout.js
-import React from 'react';
-import { Provider } from 'react-redux';
-import store from './store';
-import './global.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import RootLayout from './rootlayout';
+// /app/layout.js
 
-export default function MainLayout({ children }) {
+'use client'
+import React, { useEffect, useState } from 'react'
+import Sidebar from './component/sidebar/sidebar'
+import AuthModal from './component/auth-modal/page'
+import $ from 'jquery';
+import Popper from 'popper.js';
+import './global.css'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import dynamic from 'next/dynamic'
+
+export default function RootLayout({ children }) {
+  const BootstrapJS = dynamic(() => import('bootstrap/dist/js/bootstrap.bundle.min.js'), { ssr: false });
+  useEffect(() => {
+    BootstrapJS();
+  }, []);
+  const [show, setShow] = useState(false);
+  const [darkThemeMode, setDarkThemeMode] = useState(false);
+  const [auth, setAuth] = useState(false);
+
+  const handleClose = () => {
+    setShow(false);
+  };
+
+  const handleShow = () => {
+    auth ? setShow(false) : setShow(true);
+  };
+  const darkMode = () => {
+    setDarkThemeMode(true);
+  }
   return (
-    <Provider store={store}>
-      <RootLayout>{children}</RootLayout>
-    </Provider>
-  );
+    <html lang="en">
+      <body className={darkThemeMode ? 'dark-mode' : 'light-mode'}>
+        <div className="main-wrapper d-flex">
+          <Sidebar showModal={handleShow} darkModeHandle={darkMode} />
+          {children}
+          <AuthModal showModal={show} handleCloseAuthModal={handleClose} />
+        </div>
+      </body>
+    </html>
+  )
 }
