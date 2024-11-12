b8b90c2554fa762016bcb70035426658c619cc7e
aqibibrahim91 <aqibibrahimepggmail.com>
2024-11-12 16:03:24 +0500

    feat: add multilingual and sicial login

---
+++ i18/index.js
@@ -0,0 +1,17 @@
+// src/i18n/index.js
+import en from "./en.json";
+import fr from "./fr.json";
+import am from "./am.json";
+import ke from "./ke.json"
+import ar from "./ar.json";
+import it from "./it.json";
+const translations = {
+    en,
+    fr,
+    am,
+    ke,
+    it,
+    ar
+};
+
+export default translations;
--- store/store.js
+++ store/store.js
@@ -2,11 +2,13 @@ import { configureStore } from '@reduxjs/toolkit';
 import authReducer from '../features/auth/authSlice';
 import apiReducer from '../features/apiSlice';
 import modalReducer from '../features/modalSlice';
+import languageReducer from '../features/languageSlice';
 const store = configureStore({
     reducer: {
         auth: authReducer,
         api: apiReducer,
         modal: modalReducer,
+        language: languageReducer,
     },
 });
 
